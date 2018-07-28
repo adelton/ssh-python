3fd7ba56487636239dbbcf8686ba3640ae53a476
Pan <zuboci@yandex.com>
2018-07-28 23:34:46 +0100

    Added python build ssh implementation, updated setup.py

---
+++ _setup_libssh.py
@@ -0,0 +1,21 @@
+from subprocess import check_call
+import os
+from glob import glob
+from shutil import copy2
+
+
+def build_ssh():
+    if os.path.exists('/usr/local/opt/openssl'):
+        os.environ['OPENSSL_ROOT_DIR'] = '/usr/local/opt/openssl'
+
+    if not os.path.exists('src'):
+        os.mkdir('src')
+
+    os.chdir('src')
+    check_call('cmake -DCMAKE_BUILD_TYPE=Release ../libssh',
+               shell=True, env=os.environ)
+    check_call(['make', '-j6'])
+    os.chdir('..')
+
+    for src in glob('src/src/libssh.so*'):
+        copy2(src, 'ssh/')
